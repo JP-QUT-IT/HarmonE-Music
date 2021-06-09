3b3baefdd5826f64cdea4ebec8b74c1e96ec0302
Roozza <rory.blair@connect.qut.edu.au>
2021-06-09 15:38:56 +1000

    Form Changes

--- travel/forms.py
+++ travel/forms.py
@@ -1,53 +1,61 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, SelectField, RadioField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, IntegerField, DateField
+from wtforms.validators import InputRequired, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 #add the types of files allowed as a set
 ALLOWED_FILE = {'png', 'jpg', 'JPG', 'PNG'}
 
-class DestinationForm(FlaskForm):
-  name = StringField('Country', validators=[InputRequired()])
-  # adding two validators, one to ensure input is entered and other to check if the 
-  #description meets the length requirements
-  description = TextAreaField('Description', 
-            validators=[InputRequired()])
-  #create a filefield that takes two validators - File required and File Allowed
-  image = FileField('Destination Image', validators=[FileRequired(message='Image can not be empty'),
-                                         FileAllowed(ALLOWED_FILE, message='Only support png, jpg, JPG, PNG, bmp')])
-   
-  currency = StringField('Currency', validators=[InputRequired()])
+class EventForm(FlaskForm):
+  name = StringField('Event Title', validators=[InputRequired()])
+  image = FileField('Event Advertisement Image', validators=[FileRequired(message='Image can not be empty'),
+          FileAllowed(ALLOWED_FILE, message='Only support png, jpg, JPG, PNG, bmp')])
+  genre = StringField('Event Genre', validators=[InputRequired()])
+  description = TextAreaField('Event Description', validators=[InputRequired()])
+  venue = StringField('Event Venue', validators=[InputRequired()])
+  start = StringField('Event Start', validators=[InputRequired()])
+  end = StringField('Event End', validators=[InputRequired()])
+  tickets = IntegerField('Initial Tickets Available', validators=[InputRequired()])
+  status = SelectField('Event Status', choices=[('upcoming', 'UPCOMING'), ('inactive', 'INACTIVE'), ('booked', 'BOOKED'), ('cancelled', 'CANCELLED')])
   submit = SubmitField("Create")
 
 
-class EditDestinationForm(FlaskForm):
-  name = StringField('Country', validators=[InputRequired()])
-  # adding two validators, one to ensure input is entered and other to check if the 
-  #description meets the length requirements
-  description = TextAreaField('Description', 
-            validators=[InputRequired()])
-  #create a filefield that takes two validators - File required and File Allowed
-  image = FileField('Destination Image', validators=[FileRequired(message='Image can not be empty'),
-                                         FileAllowed(ALLOWED_FILE, message='Only support png, jpg, JPG, PNG, bmp')])
-   
-  currency = StringField('Currency', validators=[InputRequired()])
-  submit = SubmitField("Create")
+class EditEventForm(FlaskForm):
+  name = StringField('Event Title', validators=[InputRequired()])
+  image = FileField('Event Advertisement Image', validators=[FileRequired(message='Image can not be empty'),
+          FileAllowed(ALLOWED_FILE, message='Only support png, jpg, JPG, PNG, bmp')])
+  genre = StringField('Event Genre', validators=[InputRequired()])
+  description = TextAreaField('Event Description', validators=[InputRequired()])
+  venue = StringField('Event Venue', validators=[InputRequired()])
+  start = StringField('Event Start', validators=[InputRequired()])
+  end = StringField('Event End', validators=[InputRequired()])
+  tickets = IntegerField('Initial Tickets Available', validators=[InputRequired()])
+  status = SelectField('Event Status', choices=[('upcoming', 'UPCOMING'), ('inactive', 'INACTIVE'), ('booked', 'BOOKED'), ('cancelled', 'CANCELLED')])
+  submit = SubmitField("Edit")
   
   #this should already be there in the forms.py
   
 class CommentForm(FlaskForm):
     text = TextAreaField('Comment', [InputRequired()])
-    submit = SubmitField('Create')
+    submit = SubmitField('Post')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',validators=[InputRequired("Please enter your username")])
+    companyname = StringField('Username',validators=[InputRequired("Please enter your username")])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
 
-class RegisterForm(FlaskForm):
+class CustomerRegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     email = StringField('Email', validators=[Email("Please enter an email address")])
     password = PasswordField('Password', validators=[InputRequired(),
         EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
-    submit = SubmitField('Register')
+    submit = SubmitField('Register As Customer')
+
+class AdminRegisterForm(FlaskForm):
+    companyname = StringField('Company Name', validators=[InputRequired()])
+    email = StringField('Email', validators=[Email("Please enter an email address")])
+    password = PasswordField('Password', validators=[InputRequired(),
+        EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password")
+    submit = SubmitField('Register As Administrator')
