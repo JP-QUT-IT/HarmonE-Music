d85f5ead4f5534d1cb9bc11ab24c7df22af3b67e
JCKH <josh.c.hill86@gmail.com>
2021-06-14 02:55:15 +1000

    book.html now accessible, minor formatting updates to book.html

--- music/events.py
+++ music/events.py
@@ -1,6 +1,8 @@
 ## Import Necessary Modules ##
+from werkzeug.utils import secure_filename
+import os
 import re
-from flask import ( 
+from flask import (
     Blueprint, app, flash, render_template, request, url_for, redirect
 )
 from sqlalchemy.sql.schema import FetchedValue
@@ -17,131 +19,135 @@ import math
 bp = Blueprint('event', __name__, url_prefix='/events')
 
 ## Showing Event Function and Page Creation ##
-@bp.route('/<id>') 
-def show(id): 
-  event = MusicEvent.query.filter_by(id=id).first()  
-  cform = CommentForm()
-  return render_template('events/show.html', event=event, form=cform)
+
+
+@bp.route('/<id>')
+def show(id):
+    event = MusicEvent.query.filter_by(id=id).first()
+    cform = CommentForm()
+    return render_template('events/show.html', event=event, form=cform)
 
 ## Creating Event Function and Page Creation ##
-@bp.route('/create', methods=['GET','POST'])
+
+
+@bp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
 
-  if (current_user.role == 'admin'):
-    pass
-  elif (current_user.role == 'customer'):
-    return redirect('/Forbidden')
-  else:
-    return redirect('/Forbidden')
-  
-  print('Method type: ', request.method)
+    if (current_user.role == 'admin'):
+        pass
+    elif (current_user.role == 'customer'):
+        return redirect('/Forbidden')
+    else:
+        return redirect('/Forbidden')
 
-  form = EventForm()
-  if(form.validate_on_submit()):
-    db_file_path=check_upload_file(form)
-    event=MusicEvent(
-    EventName=form.name.data, 
-    EventCreator=current_user.name,
-    EventImage=db_file_path, 
-    EventGenre=form.genre.data,
-    EventDescription=form.description.data, 
-    EventVenue=form.venue.data, 
-    EventStart=form.start.data, 
-    EventEnd=form.end.data, 
-    EventTickets=form.tickets.data, 
-    EventStatus=form.status.data)
-    db.session.add(event)
-    db.session.commit()
-    flash('Created event')
-    return redirect('/')
-  return render_template('events/create.html', form=form)
+    print('Method type: ', request.method)
+
+    form = EventForm()
+    if(form.validate_on_submit()):
+        db_file_path = check_upload_file(form)
+        event = MusicEvent(
+            EventName=form.name.data,
+            EventCreator=current_user.name,
+            EventImage=db_file_path,
+            EventGenre=form.genre.data,
+            EventDescription=form.description.data,
+            EventVenue=form.venue.data,
+            EventStart=form.start.data,
+            EventEnd=form.end.data,
+            EventTickets=form.tickets.data,
+            EventStatus=form.status.data)
+        db.session.add(event)
+        db.session.commit()
+        flash('Created event')
+        return redirect('/')
+    return render_template('events/create.html', form=form)
 
 
 ## Editing Event Function and Page Creation ##
 @bp.route('/edit/<id>', methods=['GET', 'POST'])
 @login_required
 def edit(id):
-  selectedEvent = MusicEvent.query.filter_by(id = id).first()
+    selectedEvent = MusicEvent.query.filter_by(id=id).first()
 
-  if (current_user.name==selectedEvent.EventCreator):
-    pass
-  elif (current_user.role == 'customer'):
-    return redirect('/Forbidden')
-  else:
-    return redirect('/Forbidden')
+    if (current_user.name == selectedEvent.EventCreator):
+        pass
+    elif (current_user.role == 'customer'):
+        return redirect('/Forbidden')
+    else:
+        return redirect('/Forbidden')
 
-  form = EditEventForm(name=selectedEvent)
-  if form.validate_on_submit():
-    db_file_path=check_upload_file(form)
-    selectedEvent.EventName=form.name.data
-    selectedEvent.EventImage=db_file_path
-    selectedEvent.EventGenre=form.genre.data
-    selectedEvent.EventDescription=form.description.data
-    selectedEvent.EventVenue=form.venue.data
-    selectedEvent.EventStart=form.start.data
-    selectedEvent.EventEnd=form.end.data
-    selectedEvent.EventTickets=form.tickets.data
-    selectedEvent.EventStatus=form.status.data
-    db.session.commit()
-    flash('Edited event')
-    return redirect('/')
-  elif request.method == 'GET':
-    form.name.data = selectedEvent.EventName
-    form.image.data = selectedEvent.EventImage
-    form.genre.data = selectedEvent.EventGenre
-    form.description.data = selectedEvent.EventDescription
-    form.venue.data = selectedEvent.EventVenue
-    form.start.data = selectedEvent.EventStart
-    form.end.data = selectedEvent.EventEnd
-    form.tickets.data = selectedEvent.EventTickets
-    form.status.data = selectedEvent.EventStatus
-  return render_template('events/edit.html', form=form)
+    form = EditEventForm(name=selectedEvent)
+    if form.validate_on_submit():
+        db_file_path = check_upload_file(form)
+        selectedEvent.EventName = form.name.data
+        selectedEvent.EventImage = db_file_path
+        selectedEvent.EventGenre = form.genre.data
+        selectedEvent.EventDescription = form.description.data
+        selectedEvent.EventVenue = form.venue.data
+        selectedEvent.EventStart = form.start.data
+        selectedEvent.EventEnd = form.end.data
+        selectedEvent.EventTickets = form.tickets.data
+        selectedEvent.EventStatus = form.status.data
+        db.session.commit()
+        flash('Edited event')
+        return redirect('/')
+    elif request.method == 'GET':
+        form.name.data = selectedEvent.EventName
+        form.image.data = selectedEvent.EventImage
+        form.genre.data = selectedEvent.EventGenre
+        form.description.data = selectedEvent.EventDescription
+        form.venue.data = selectedEvent.EventVenue
+        form.start.data = selectedEvent.EventStart
+        form.end.data = selectedEvent.EventEnd
+        form.tickets.data = selectedEvent.EventTickets
+        form.status.data = selectedEvent.EventStatus
+    return render_template('events/edit.html', form=form)
 
 
 ## Comment Function and creation for selected Event ##
-@bp.route('/<event>/comment', methods = ['GET', 'POST']) 
-def comment(event):  
-    form = CommentForm()  
-    event_obj = MusicEvent.query.filter_by(id=event).first()  
-    if form.validate_on_submit():  
-      comment = Comment(
-        text=form.text.data,  
-        events=event_obj, 
-        users=current_user) 
-      db.session.add(comment) 
-      db.session.commit()  
-      flash('Your comment has been added', 'success') 
+@bp.route('/<event>/comment', methods=['GET', 'POST'])
+def comment(event):
+    form = CommentForm()
+    event_obj = MusicEvent.query.filter_by(id=event).first()
+    if form.validate_on_submit():
+        comment = Comment(
+            text=form.text.data,
+            events=event_obj,
+            users=current_user)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been added', 'success')
     return redirect(url_for('event.show', id=event))
 
 
 ## Booking Event Function and Page Creation ##
-@bp.route('/book/<id>', methods = ['GET', 'POST']) 
+@bp.route('/book/<id>', methods=['GET', 'POST'])
 @login_required
-def book(event):  
-    form = OrderForm() 
+def book(id):
+    event_obj = MusicEvent.query.filter_by(id=id).first()
     if (current_user.role == 'customer'):
-      pass
+        pass
     elif (current_user.role == 'admin'):
-      return redirect('/Forbidden')
+        return redirect('/Forbidden')
     else:
-      return redirect('/Forbidden') 
-    event_obj = MusicEvent.query.filter_by(id=event).first()  
-    if form.validate_on_submit():  
-      order = Order(
-        quantity=form.quantity.data,  
-        events=event_obj, 
-        users=current_user) 
-      db.session.add(order) 
-      db.session.commit() 
+        return redirect('/Forbidden')
 
-      flash('Your comment has been added', 'success') 
-
-    return redirect(url_for('event.show', id=event))
+    print('Method type: ', request.method)
+    form = OrderForm()
+    if(form.validate_on_submit()):
+        order = Order(
+            quantity=form.quantity.data,
+            events=event_obj,
+            users=current_user)
+        db.session.add(order)
+        db.session.commit()
+        return redirect('/')
+    return render_template('events/book.html', form=form)
 
 
 ## What the team wants the book page and function to do but doesn't work as there are errors with SQL data types and Python data types ##
-# @bp.route('/book/<id>', methods = ['GET', 'POST']) 
+# @bp.route('/book/<id>', methods = ['GET', 'POST'])
 # @login_required
 # def book(id):
 
@@ -152,14 +158,14 @@ def book(event):
 #     return redirect('/Forbidden')
 #   else:
 #     return redirect('/Forbidden')
-  
+
 #   print('Method type: ', request.method)
 #   form = OrderForm()
 #   if form.validate_on_submit():
 #     selectedEvent = MusicEvent.query.filter_by(id = id).first()
 #     order = Order(
-#         quantity=form.quantity.data,  
-#         events=event_obj, 
+#         quantity=form.quantity.data,
+#         events=event_obj,
 #         users=current_user)
 #     db.session.add(order)
 #     PurchaseQuantity = math.ceil(form.quantity)
@@ -170,8 +176,8 @@ def book(event):
 #     elif PurchaseQuantity == selectedEvent.EventTickets:
 #       selectedEvent.EventTickets = '0'
 #       selectedEvent.EventStatus = 'Booked Out'
-#       db.session.commit() 
-     
+#       db.session.commit()
+
 #     flash('Event is booked')
 #     return redirect('/')
 #   return render_template('events/book.html', form=form)
@@ -188,14 +194,15 @@ def book(event):
 
 
 ## File Upload Function ##
-import os
-from werkzeug.utils import secure_filename
-def check_upload_file(form):
-  fp=form.image.data
-  filename=fp.filename
-  BASE_PATH=os.path.dirname(__file__)
 
-  upload_path=os.path.join(BASE_PATH,'static/images/event',secure_filename(filename))
-  db_upload_path='/static/images/event/' + secure_filename(filename)
-  fp.save(upload_path)
-  return db_upload_path
+
+def check_upload_file(form):
+    fp = form.image.data
+    filename = fp.filename
+    BASE_PATH = os.path.dirname(__file__)
+
+    upload_path = os.path.join(
+        BASE_PATH, 'static/images/event', secure_filename(filename))
+    db_upload_path = '/static/images/event/' + secure_filename(filename)
+    fp.save(upload_path)
+    return db_upload_path
